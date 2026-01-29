bc54bf9d5fff8ed53075b68be453e12d0ebd71d1
lucky <luckyorionuser@gmail.com>
2026-01-29 13:10:44 +0100

    added node type and node structure

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+typedef enum NodeType {
+	BLOCK,
+}NodeType;
+
+typedef struct Node{
+	NodeType type;
+}Node;
+
+void create_node_block(Node* node){
+	node->type = BLOCK;
+}
 int main(){
 	printf("hello world\n");
 	return 0;
