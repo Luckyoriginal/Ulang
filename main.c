3b7989de7c6627dda908b8dab977a3547a9066d5
lucky <luckyorionuser@gmail.com>
2026-01-29 13:22:55 +0100

    added constant node

--- main.c
+++ main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
 typedef enum NodeType {
-	BLOCK,
+	CONSTANT,
 }NodeType;
 
 typedef struct Node{
 	NodeType type;
+	union {
+		float value;
+	};
 }Node;
 
-void create_node_block(Node* node){
-	node->type = BLOCK;
+void create_node_constant(Node* node, float value){
+	node->type = CONSTANT;
+	node->value = value;
+}
+
+void print_ast(Node root){
+	if (root.type==CONSTANT){
+		printf("CONSTANT: %f\n",root.value);
+	}
 }
 int main(){
 	printf("hello world\n");
+	Node root;
+	create_node_constant(&root,3);
+	print_ast(root);
 	return 0;
 }
