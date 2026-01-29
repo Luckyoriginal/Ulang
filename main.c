815e3263bb22dcac920d2935eb93cbdc40db8080
lucky <luckyorionuser@gmail.com>
2026-01-29 13:28:09 +0100

    added constant type

--- main.c
+++ main.c
@@ -4,23 +4,39 @@ typedef enum NodeType {
 	CONSTANT,
 }NodeType;
 
+typedef enum ConstantType {
+	FLOAT, INT, STRING, CHAR
+}ConstantType;
+
+typedef struct Constant {
+	ConstantType type;
+	union {
+		float Float;
+		int Int;
+		char* String;
+		char Char;
+	};
+}Constant;
+
 typedef struct Node{
 	NodeType type;
 	union {
-		float value;
+		Constant constant;
 	};
 }Node;
 
 void create_node_constant(Node* node, float value){
 	node->type = CONSTANT;
-	node->value = value;
+	node->constant.type = FLOAT;
+	node->constant.Float = value;
 }
 
 void print_ast(Node root){
 	if (root.type==CONSTANT){
-		printf("CONSTANT: %f\n",root.value);
+		printf("CONSTANT: %f\n",root.constant.Float);
 	}
 }
+
 int main(){
 	printf("hello world\n");
 	Node root;
